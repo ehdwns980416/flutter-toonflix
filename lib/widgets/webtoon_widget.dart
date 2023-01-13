526478af14df5ca7299391443414451baf489e7b
ehdwns0416 <end75814@gmail.com>
2023-01-13 16:31:09 +0900

    Navigation 및 제스처 구현

---
+++ lib/widgets/webtoon_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/screens/detail_screen.dart';
+
+class Webtoon extends StatelessWidget {
+  final String title, thumb, id;
+
+  const Webtoon({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                DetailScreen(title: title, thumb: thumb, id: id),
+          ),
+        );
+        print('take me homeee');
+      },
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(.5)),
+                ]),
+            clipBehavior: Clip.hardEdge,
+            width: 250,
+            child: Image.network(thumb),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
